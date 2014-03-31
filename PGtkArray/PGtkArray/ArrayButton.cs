46305942450d9a918de4eb52f464856ddc1fec48
luisvelascoserpis <luis.velasco.serpis@gmail.com>
2014-03-31 12:56:35 +0200

    ArrayButton con propiedades indizadas y SetLabels

--- PGtkArray/PGtkArray/ArrayButton.cs
+++ PGtkArray/PGtkArray/ArrayButton.cs
@@ -1,33 +1,87 @@
 using Gtk;
 using System;
+using System.Collections.Generic;
 
 namespace PGtkArray
 {
 	[System.ComponentModel.ToolboxItem(true)]
 	public partial class ArrayButton : Gtk.Bin
 	{
+		
 		private int rows = 1;
 		private int columns = 1;
 		
 		private Table table;
 		
+		private List<Button> buttons;
+		
 		public ArrayButton ()
 		{
 			this.Build ();
 			
-			table = new Table((uint)rows, (uint)columns, true);
-			Add (table);
-			
-			refresh();
+			refresh ();
 		}
 		
-		
 		private void refresh() {
-			Button button = new Button();
-			button.Visible = true;
-			button.Label = "33";
-			table.Attach (button, 0, 1, 0 ,1);
-			//...
+			if (table != null)
+				Remove (table);
+			
+			table = new Table((uint)rows, (uint)columns, true);
+			table.Visible = true;
+			Add (table);
+			
+			buttons = new List<Button>();
+			
+			for (uint row = 0; row < rows; row++)
+				for (uint column = 0; column < columns; column++) {
+					Button button = new Button();
+					button.Visible = true;
+					button.Label = string.Format ("[{0}, {1}]", row, column);
+					table.Attach (button, column, column + 1, row, row + 1);
+					buttons.Add (button);
+				}
+		}
+		
+		public int Rows {
+			get {return rows;}
+			set {
+				rows = value;
+				refresh ();
+			}
+		}
+		
+		public int Columns {
+			get {return columns;}
+			set {
+				columns = value;
+				refresh ();
+			}
+		}
+		
+		public Button Get(int index) {
+			return buttons[index];
+		}
+		
+		public Button this[int index] {
+			get {return buttons[index];}
+		}
+		
+		public Button Get(int row, int column) {
+			return buttons[row * columns + column];
+		}
+		
+		public Button this[int row, int column] {
+			get {return buttons[row * columns + column];}
+		}
+		
+		//Establece las etiquetas indicadas
+		public void SetLabels(string[] labels) {
+			for (int index = 0; index < buttons.Count; index++) {
+				if (index < labels.Length)
+					buttons[index].Label = labels[index];
+				buttons[index].Visible = index < labels.Length;
+			}
+			
 		}
 	}
 }
--- PGtkArray/PGtkArray/MainWindow.cs
+++ PGtkArray/PGtkArray/MainWindow.cs
@@ -6,6 +6,21 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		
+		Gdk.Color greenColor = new Gdk.Color(0, 255, 0);
+		
+		int count = arrayButton.Rows * arrayButton.Columns;
+		for (int index = 0; index < count; index++) {
+			arrayButton.Get (index).Label = string.Format ("{0}", index + 1);
+			if (index % 2 == 0)
+				arrayButton[index]. ModifyBg (StateType.Normal, greenColor);
+		}
+		
+		arrayButton.Get(2, 4).Label = "*";
+		arrayButton[3, 5].Label = "**";
+		
+		arrayButton.SetLabels (new string[]{"Uno", "Dos", "Tres"});
+		arrayButton.SetLabels (new string[]{"Uno", "Dos", "Tres", "Cuatro"});
 	}
 	
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
